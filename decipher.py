0942c72c58261b3616615f7d9640f8c62779705b
khemritolya <khemritolya@gmail.com>
2016-04-30 18:56:26 -0400

    Update decipher.py
    Added support for numbers

--- decipher.py
+++ decipher.py
@@ -1,6 +1,6 @@
 #
 # decipher.py (c) Luis Hoderlein
-# 
+#
 # BUILT: Apr 21, 2016
 #
 # This program can brute force Cesarian ciphers
@@ -22,6 +22,8 @@ raw_txt = raw_input("Enter ciphertext: ")
 raw_int = []
 txt = ""
 spaces = []
+periods = []
+excl = []
 
 # make all lower case (necessary)
 raw_txt = raw_txt.lower()
@@ -33,24 +35,52 @@ for i in range(0, len(raw_txt)):
 	else:
 		spaces.append(i);
 
-# turn chars into ints 
-for i in range(0, len(txt)):	
-	raw_int.append(string.lowercase.index(txt[i]))
+# log periods and remove them
+raw_txt = txt
+txt = ""
+for i in range(0,len(raw_txt)):
+	if raw_txt[i] != ".":
+		txt = txt + raw_txt[i]
+	else:
+		periods.append(i);
 
-# loop through every possible solution (26 of them), using i has cipher number
-# and print all possible solution + add the spaces again
+# log exclamation marks and remove them
+raw_txt = txt
+txt = ""
+for i in range(0,len(raw_txt)):
+	if raw_txt[i] != "!":
+		txt = txt + raw_txt[i]
+	else:
+		excl.append(i);
+
+# turn chars into ints
+for i in range(0, len(txt)):
+	try:
+		raw_int.append(26 + int(float(txt[i])))
+	except ValueError:
+		raw_int.append(string.lowercase.index(txt[i]))
+
+# loop through every possible solution (36 of them), using i has cipher number
+# and print all possible solution + add the spaces, exclamtion marks, perdiods, question marks again
 # to prevent some weird bug, possible int has to be reassigned every time
-for i in range(0, 26):
+for i in range(0, 36):
 	possible_int = []
 	for j in range(0, len(raw_int)):
 		possible_int.append(raw_int[j])
-	possible_txt = ""
+	possible_txt = ''
 	for j in range(0, len(possible_int)):
 		possible_int[j] = possible_int[j]+i
-		if possible_int[j] >= 26:
-			possible_int[j] = possible_int[j] - 26
-		possible_txt = possible_txt + string.lowercase[possible_int[j]]
+		if possible_int[j] >= 36:
+			possible_int[j] = possible_int[j] - 36
+		if possible_int[j] < 26:
+			possible_txt = possible_txt + string.lowercase[possible_int[j]]
+		else:
+			possible_txt = possible_txt + str((possible_int[j] - 26))
 	del possible_int
 	for j in range(0, len(spaces)):
-		possible_txt = possible_txt[:spaces[j]] + " " +possible_txt[spaces[j]:]	
+		possible_txt = possible_txt[:spaces[j]] + " " +possible_txt[spaces[j]:]
+	for j in range(0, len(periods)):
+		possible_txt = possible_txt[:periods[j]] + "." +possible_txt[periods[j]:]
+	for j in range(0, len(excl)):
+		possible_txt = possible_txt[:excl[j]] + "!" +possible_txt[excl[j]:]	
 	print "Solution "+pad(i)+" is: "+possible_txt
